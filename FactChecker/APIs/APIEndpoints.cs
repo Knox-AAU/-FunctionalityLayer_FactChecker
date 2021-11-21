0b1d008c4e29e7d778d2675e29ba86eb02c34dc6
Maggedelle <33657258+Maggedelle@users.noreply.github.com>
2021-11-21 16:49:28 +0100

    Added a static class which holds references to all knox endpoints
    Added a static class which holds references to all knox endpoints

---
+++ FactChecker/APIs/APIEndpoints.cs
@@ -0,0 +1,15 @@
+﻿namespace FactChecker.APIs
+{
+    //This class includes a reference to all needed KNOX-endpoints
+    public static class APIEndpoints
+    {
+
+        //Search API
+        public static string wordcount = "http://knox-node02.srv.aau.dk/wordcount/";
+        public static string wordratio = "http://knox-node02.srv.aau.dk/wordratio/";
+
+        //Lemmatizer
+        public static string lemmatizer = "http://knox-master01.srv.aau.dk/lemmatizer";
+
+    }
+}
