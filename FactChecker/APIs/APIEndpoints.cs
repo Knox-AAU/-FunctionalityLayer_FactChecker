9227b7d896d435c07baac78c005e1e1fb747bdbe
Maggedelle <33657258+Maggedelle@users.noreply.github.com>
2021-11-21 17:11:00 +0100

    Added a HTTP get request

--- FactChecker/APIs/APIEndpoints.cs
+++ FactChecker/APIs/APIEndpoints.cs
@@ -1,15 +1,36 @@
-﻿namespace FactChecker.APIs
+﻿using System;
+using System.Net;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Threading.Tasks;
+
+
+namespace FactChecker.APIs
 {
-    //This class includes a reference to all needed KNOX-endpoints
     public static class APIEndpoints
     {
 
         //Search API
-        public static string wordcount = "http://knox-node02.srv.aau.dk/wordcount/";
-        public static string wordratio = "http://knox-node02.srv.aau.dk/wordratio/";
+        public static string wordCountURL = "http://knox-node02.srv.aau.dk/wordcount/";
+        public static string wordRatioURL = "http://knox-node02.srv.aau.dk/wordratio/";
 
         //Lemmatizer
-        public static string lemmatizer = "http://knox-master01.srv.aau.dk/lemmatizer";
+        public static string lemmatizerURL = "http://knox-master01.srv.aau.dk/lemmatizer";
 
+
+        static HttpClient client = new HttpClient();
+
+
+        public static async Task<WordRatio> GetWordRatio (string[] terms)
+        {
+            WordRatio articles = null;
+            HttpResponseMessage response = await client.GetAsync(APIEndpoints.wordCountURL);
+            if (response.IsSuccessStatusCode)
+            {
+                articles = await response.Content.ReadAsAsync<WordRatio>();
+            }
+
+            return articles;
+        }
     }
 }
---
+++ FactChecker/APIs/WordRatio.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Net;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Threading.Tasks;
+
+namespace FactChecker.APIs
+{
+
+    public class WordRatio
+    {
+        public int count;
+        public string path;
+        //TODO: Make this class fit the returned object
+    }
+}
