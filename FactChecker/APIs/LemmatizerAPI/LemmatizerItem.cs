22dd1f9fbc02101b76b2da08c105f14afb2abb74
Magnus Axelsen <maxels19@student.aau.dk>
2021-11-23 12:33:04 +0100

    Added lemmatizer endpoint

---
+++ FactChecker/APIs/LemmatizerAPI/LemmatizerItem.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace FactChecker.APIs.LemmatizerAPI
+{
+    public class LemmatizerItem
+    {
+        public string lemmatized_string;
+    }
+}
