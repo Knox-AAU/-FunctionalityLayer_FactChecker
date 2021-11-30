3d7898086be364c442e7d085e5264597f5f20332
Kasper Masznicz <kmaszn19@student.aau.dk>
2021-11-30 12:26:31 +0100

    added regex to remove noise

--- Wordcount/wordcount.py
+++ Wordcount/wordcount.py
@@ -1,6 +1,7 @@
 from bs4 import BeautifulSoup
 import requests
 import json
+import re
 
 
 def wordCount(word, article):
@@ -9,7 +10,9 @@ def wordCount(word, article):
     soup = BeautifulSoup(req.text, "lxml")
     clean_text = soup.get_text().split("Jump to search")[1]
     clean_text = clean_text.split(
-        "Retrieved from \"https://en.wikipedia.org/w/index.php?title=")[0]
+        "References[edit]")[0]
+
+    clean_text = re.sub("(\[.*?\])", '', clean_text)
 
     text = {
         "string": clean_text,
@@ -19,5 +22,7 @@ def wordCount(word, article):
     lemmatized_string = requests.post(
         f"http://127.0.0.1:5000/", data=textJson)
 
-    lemma = str(lemmatized_string.content.decode(encoding='UTF-8')).split(" ")
+    lemma = str(lemmatized_string.content.decode(encoding='UTF-8'))
+    lemma = re.sub("\n", ' ', lemma)
+    lemma = re.sub("\\\\", ' ', lemma).split(" ")
     return lemma.count(word)
