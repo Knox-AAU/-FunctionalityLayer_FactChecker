a605adc621864da14466394fd70a641d23be2905
Magnus Axelsen <maxels19@student.aau.dk>
2021-12-06 10:01:14 +0100

    Use labels

--- FactChecker/APIs/KnowledgeGraphAPI/KnowledgeGraphItem.cs
+++ FactChecker/APIs/KnowledgeGraphAPI/KnowledgeGraphItem.cs
@@ -7,14 +7,21 @@ namespace FactChecker.APIs.KnowledgeGraphAPI
 {
     public class KnowledgeGraphItem
     {
-        public string s;
-        public string r;
-        public string t;
+        public string s { get; set; }
+        public string r { get; set; }
+        public string t { get; set; }
+        public bool showPassages { get; set; }
         public KnowledgeGraphItem (string s, string r, string t)
         {
             this.s = s;
             this.r = r;
             this.t = t;
+            showPassages = false;
+        }
+
+        public KnowledgeGraphItem()
+        {
+            showPassages = false;
         }
 
         public override string ToString()
