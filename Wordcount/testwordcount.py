aa8b24758b90a87e615ee7b79e474d7c84e850f1
Kasper Masznicz <kmaszn19@student.aau.dk>
2021-12-08 14:35:45 +0100

    webscrapertest added and some refactoring

--- Wordcount/testwordcount.py
+++ Wordcount/testwordcount.py
@@ -1,17 +1,25 @@
 import unittest
 from wordcount import articleStore
 
+
 class wordcountTest(unittest.TestCase):
 
     def testWebscraper(self):
         self.assertIsNotNone(articleStore(
-            1, "https://en.wikipedia.org/wiki/Eternals_(film)"))
+            "https://en.wikipedia.org/wiki/Eternals_(film)"))
 
-    def testWebscraper(self):
-        self.assertIsNotNone(
-            articleStore(
-            1, "https://en.wikipedia.org/wiki/Eternals_(film)"))
+    def testArticleInTuple(self):
+        self.assertIsInstance(articleStore(
+            "https://en.wikipedia.org/wiki/Eternals_(film)")[0], str)
 
+    def testLenghtInTuple(self):
+        self.assertIsInstance(articleStore(
+            "https://en.wikipedia.org/wiki/Eternals_(film)")[1], int)
+
+    def testUniqueLenght(self):
+        testdata = articleStore(
+            "https://en.wikipedia.org/wiki/Eternals_(film)")
+        self.assertLessEqual(testdata[2], testdata[1])
 
 
 if __name__ == '__main__':
