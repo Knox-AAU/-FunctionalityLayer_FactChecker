3aa8bb1553c17a880285fd8ae8dd22ec32b6f909
Magnus Axelsen <maxels19@student.aau.dk>
2021-12-14 11:10:20 +0100

    Update Startup.cs

--- FactChecker/Startup.cs
+++ FactChecker/Startup.cs
@@ -51,12 +51,11 @@ namespace FactChecker
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
-            if (env.IsDevelopment())
-            {
+          
                 app.UseDeveloperExceptionPage();
                 app.UseSwagger();
                 app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "FactChecker v1"));
-            }
+            
 
             app.UseHttpsRedirection();
 
