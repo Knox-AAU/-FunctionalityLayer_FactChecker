4baa087e36f5e81b50edb50576cbc3691dd770af
Blank <47646799+BlankWheein@users.noreply.github.com>
2022-09-26 16:28:18 +0200

    inint

---
+++ FactChecker/Levenshtein/Levenshtein.cs
@@ -0,0 +1,54 @@
+﻿using System;
+namespace FactChecker.Levenshtein
+{
+
+    public class LevenshteinDistanceAlgorithm
+    {
+        //Create a matrix that returns the distance between two strings
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="target"></param>
+        /// <param name="source"></param>
+        /// <returns></returns>
+        public static int LevenshteinDistance(string target, string source)
+        {
+            double[,] matrix = new double[target.Length + 1, source.Length + 1];
+
+            for (int i = 0; i < matrix.GetLength(0); i++)
+            {
+                matrix[i, 0] = i;
+            }
+
+            for (int i = 0; i < matrix.GetLength(1); i++)
+            {
+                matrix[0, i] = i;
+            }
+            for (int row = 1; row < matrix.GetLength(0); row = row + 1)
+            {
+                for (int col = 1; col < matrix.GetLength(1); col = col + 1)
+                {
+
+                    //charaters are same
+                    if (source[col - 1] == target[row - 1])
+                    {
+                        matrix[row, col] = matrix[row - 1, col - 1];
+                    }
+
+
+                    //charaters are different
+                    else
+                    {
+                        matrix[row, col] = Math.Min(matrix[row, col - 1], Math.Min(
+                            matrix[row - 1, col], matrix[row - 1, col - 1])) + 1;
+                    }
+                }
+            }
+
+            double result = matrix[matrix.GetLength(0) - 1, matrix.GetLength(1) - 1];
+            return (int)result;
+        }
+
+
+    }
+}
