5254513ea492b35714da34fe26d81b05908a48a1
SebxGit <minhseb@gmail.com>
2022-10-06 13:12:15 +0200

    Added SimRank to main

---
+++ FactChecker/SimRank/Node.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections.Generic;
+
+namespace FactChecker.SimRank
+{
+    public partial class SimRank
+    {
+        class Node
+        {
+            public string data { get; set; }
+            public List<Node> children = new();
+            public List<Node> parents = new();
+            public Node(string data)
+            {
+                this.data = data;
+            }
+        }
+
+
+    }
+
+
+
+}
