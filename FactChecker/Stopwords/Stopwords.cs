cacc05b00209cc9089a59a8365f96c0868c4c9d6
Casper Andersen <casper11andersen@gmail.com>
2022-10-11 11:53:47 +0200

    stop word class can now change between danish and english

--- FactChecker/Stopwords/Stopwords.cs
+++ FactChecker/Stopwords/Stopwords.cs
@@ -5,20 +5,33 @@ using System.Threading.Tasks;
 
 namespace FactChecker.Stopwords
 {
+    public enum Stopwords_Language{
+        Danish = 0,
+        English = 1
+    }
     public class Stopwords
     {
         public Dictionary<string,string> stopwords = new();
         public HashSet<string> stopwords_hashset = new();
-        public Stopwords ()
+        private Stopwords_Language stopwords_language {get; set;}
+        public Stopwords (Stopwords_Language language = Stopwords_Language.English)
         {
+            stopwords_language = language;
             GetStopWords();
         }
 
         public async void GetStopWords()
         {
+            List<string> words = new();
             IO.FileStreamHandler fileStreamHandler = new();
-            List<string> words = await fileStreamHandler.ReadFile("./TestData/stopwords.txt");
+            if(stopwords_language == Stopwords_Language.English){
 
+                words = await fileStreamHandler.ReadFile("./TestData/stopwords.txt");
+            } else {
+                words = await fileStreamHandler.ReadFile("./TestData/danish_stopwords.txt");
+
+            }
+            
             foreach(string s in words)
             {
                 stopwords_hashset.Add(s);
