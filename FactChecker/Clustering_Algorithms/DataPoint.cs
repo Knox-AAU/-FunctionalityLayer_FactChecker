714c06f02520706791a315818de8420a6d167c9a
Casper Andersen <casper11andersen@gmail.com>
2022-11-10 10:16:27 +0100

    add clustering algos

---
+++ FactChecker/Clustering_Algorithms/DataPoint.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.ML.Data;
+namespace FactChecker.Clustering
+{
+public class DataPoint {
+    
+    [VectorType(150)]
+    public float[] Features { get; set; }
+    
+}
+public class TextData
+{
+    public string Text { get; set; }
+}
+public class Prediction
+{
+    [ColumnName("PredictedLabel")]
+    public uint PredictedClusterId;
+
+    [ColumnName("Score")]
+    public float[] Distances;
+}
+}
