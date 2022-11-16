d5a70a5d699b3e5f445fef00798a7316e3a6b352
D-Kuma-C <dberwa20@student.aau.dk>
2022-11-16 11:42:31 +0100

    benchmark updated

--- FactChecker/Jaccard/JaccardBenchmark.cs
+++ FactChecker/Jaccard/JaccardBenchmark.cs
@@ -10,54 +10,43 @@ namespace FactChecker.JaccardSim
     [MemoryDiagnoser]
     public class JaccardBenchmark
     {
-        private string __checkstring_target_long = "Holland instance of Spider-Man";
-        private string __checkstring_source_long = "the Ioniq 5 electric-powered SUV, starring Holland and Batalon, with Watts directing. The Ioniq 5 and Hyundai Tucson are featured in the film. In late February 2021, Holland, Batalon, and Zendaya released three stills featuring their characters from the film alongside fake logos with the titles Spider-Man: Phone Home, Spider-Man: Home-Wrecker, and Spider-Man: Home Slice, respectively. The film's official title was announced the next day with a video showing Holland, Batalon, and Zendaya leaving Watts' office (where they supposedly received";
+        [ParamsSource(nameof(Targets))]
+        public string Target { get; set; }
+        [ParamsSource(nameof(Sources))]
+        public string Source { get; set; }
 
-        private string __checkstring_target_short = "This repo contains LLILC, an LLVM based compiler for .NET Core. It includes a set of cross-platform .NET code generation tools that enables compilation of MSIL byte code to LLVM supported platforms.";
-        private string __checkstring_source_short = "build";
+
+        public IEnumerable<string> Targets => new[]
+        {
+            "build",
+            "Holland instance of Spider-Man",
+        };
+        public IEnumerable<string> Sources => new[]
+        {
+            "This repo contains LLILC, an LLVM based compiler for .NET Core. It includes a set of cross-platform .NET code generation tools that enables compilation of MSIL byte code to LLVM supported platforms.",
+            "the Ioniq 5 electric-powered SUV, starring Holland and Batalon, with Watts directing. The Ioniq 5 and Hyundai Tucson are featured in the film. In late February 2021, Holland, Batalon, and Zendaya released three stills featuring their characters from the film alongside fake logos with the titles Spider-Man: Phone Home, Spider-Man: Home-Wrecker, and Spider-Man: Home Slice, respectively. The film's official title was announced the next day with a video showing Holland, Batalon, and Zendaya leaving Watts' office (where they supposedly received"
+        };
 
         [Benchmark]
-        public void BenchJaccard_long()
+        public void BenchJaccard()
         {
             JaccardSim js = new();
-            js.similarity(__checkstring_target_long, __checkstring_source_long);
+            js.similarity(Target, Source);
         }
 
         [Benchmark]
-        public void BenchJaccard_short()
+        public void BenchJaccard_v2()
         {
             JaccardSim js = new();
-            js.similarity(__checkstring_target_short, __checkstring_source_short);
+            js.similarity_v2(Target, Source);
         }
 
         [Benchmark]
-        public void BenchJaccard_v2_long()
+        public void BenchJaccard_v3()
         {
             JaccardSim js = new();
-            js.similarity_v2(__checkstring_target_long, __checkstring_source_long);
+            js.similarity_v3(Target, Source);
         }
-
-        [Benchmark]
-        public void BenchJaccard_v2_short()
-        {
-            JaccardSim js = new();
-            js.similarity_v2(__checkstring_target_short, __checkstring_source_short);
-        }
-
-        [Benchmark]
-        public void BenchJaccard_v3_long()
-        {
-            JaccardSim js = new();
-            js.similarity_v3(__checkstring_target_long, __checkstring_source_long);
-        }
-
-        [Benchmark]
-        public void BenchJaccard_v3_short()
-        {
-            JaccardSim js = new();
-            js.similarity_v3(__checkstring_target_short, __checkstring_source_short);
-        }
-
 
         public static void RunBenchmarks()
         {
--- FactChecker/Overlap_Coefficient/OverlapBench.cs
+++ FactChecker/Overlap_Coefficient/OverlapBench.cs
@@ -1,5 +1,6 @@
 ﻿using BenchmarkDotNet.Attributes;
 using FactChecker.Cosine;
+using System.Collections.Generic;
 
 namespace FactChecker.Overlap_Coefficient
 {
@@ -7,24 +8,28 @@ namespace FactChecker.Overlap_Coefficient
     [MemoryDiagnoser]
     public class OverlapBench
     {
-        private string __checkstring_target_long = "Holland instance of Spider-Man";
-        private string __checkstring_source_long = "the Ioniq 5 electric-powered SUV, starring Holland and Batalon, with Watts directing. The Ioniq 5 and Hyundai Tucson are featured in the film. In late February 2021, Holland, Batalon, and Zendaya released three stills featuring their characters from the film alongside fake logos with the titles Spider-Man: Phone Home, Spider-Man: Home-Wrecker, and Spider-Man: Home Slice, respectively. The film's official title was announced the next day with a video showing Holland, Batalon, and Zendaya leaving Watts' office (where they supposedly received";
+        [ParamsSource(nameof(Targets))]
+        public string Target { get; set; }
+        [ParamsSource(nameof(Sources))]
+        public string Source { get; set; }
 
-        private string __checkstring_target_short = "This repo contains LLILC, an LLVM based compiler for .NET Core. It includes a set of cross-platform .NET code generation tools that enables compilation of MSIL byte code to LLVM supported platforms.";
-        private string __checkstring_source_short = "build";
+
+        public IEnumerable<string> Targets => new[]
+        {
+            "build",
+            "Holland instance of Spider-Man",
+        };
+        public IEnumerable<string> Sources => new[]
+        {
+            "This repo contains LLILC, an LLVM based compiler for .NET Core. It includes a set of cross-platform .NET code generation tools that enables compilation of MSIL byte code to LLVM supported platforms.",
+            "the Ioniq 5 electric-powered SUV, starring Holland and Batalon, with Watts directing. The Ioniq 5 and Hyundai Tucson are featured in the film. In late February 2021, Holland, Batalon, and Zendaya released three stills featuring their characters from the film alongside fake logos with the titles Spider-Man: Phone Home, Spider-Man: Home-Wrecker, and Spider-Man: Home Slice, respectively. The film's official title was announced the next day with a video showing Holland, Batalon, and Zendaya leaving Watts' office (where they supposedly received"
+        };
 
         [Benchmark]
         public void BenchOverlap_long()
         {
             OverlapCM oc = new();
-            oc.Similarity(__checkstring_target_long, __checkstring_source_long);
-        }
-
-        [Benchmark]
-        public void BenchOverlap_short()
-        {
-            OverlapCM oc = new();
-            oc.Similarity(__checkstring_target_short, __checkstring_source_short);
+            oc.Similarity(Target, Source);
         }
     }
 }
